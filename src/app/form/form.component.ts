51d6e5058525c26c740721785f6fc4c0a7487d35
Markmercy <mark.mercy69@gmail.com>
2019-02-17 15:26:52 +0700

    mark4

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { User } from '../user';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-form',
@@ -18,18 +19,44 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
     this.formGroup = this.formBuild.group({
-      FirstName: this.formBuild.control(''),
-      LastName: [''],
-      Email:[''],
-      Age:['21']
+      FirstName: ['' , [Validators.required, Validators.minLength(3)]],
+      LastName: ['สกุล' ,[Validators.required, Validators.minLength(3)]],
+      Email:['@Email',[this.EmilVaildator]],
+      Age:['22',[Validators.min(0), Validators.max(99)]]
     })
   }
+    EmilVaildator(control : AbstractControl){
+      const value: string =control.value;
+      if (value && value.includes('@')){
+          return null;
+      }return{
+        Email :true
+      }
+
+    }
   onSubmit(form: FormGroup){
+    console.log(form.valid , form.invalid)
+    //console.log((<FormControl>form.get('FirstName')).errors);
+    console.log(form.get('FirstName').errors);
+
     const {FirstName,LastName,Email,Age} = form.value;
     console.log(FirstName,LastName,Email,Age);
     const user = new User(FirstName,LastName,Email,Age);
     console.log(user);
-    alert(FirstName);
+    // alert(FirstName);
+    if (form.valid){
+      const{
+        FirstName,LastName,Email,Age
+      }= form.value;
+
+    }else{
+      ['FirstName','LastName','Email','Age'
+    ].forEach((key:string) => {
+      console.log(form.get(key).touched);
+      form.get(key).markAsTouched();
+      console.log(form.get(key).touched);
+    })
+    }
 
   }
 
